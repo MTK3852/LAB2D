2eb16181ee0c6fb204e527bab9c8c878f23ff9cf
MTK3852 <mtk835123@gmail.com>
2020-04-26 19:05:34 +0300

    Second Task

---
+++ LAB2.1OOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LAB2._1OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n**************************\n");
+            Console.WriteLine("X\t        Y\n");
+            Console.WriteLine("**************************\n");
+
+            double y = 0;
+            for (double x = 0;x<6;x+=0.5)
+            {
+               
+              y = 1 / (1 - Math.Sqrt(x));
+                   if(x==1)
+                {
+                    
+                    y = 0;
+                    Console.Write("(Divided by zero exception y=Infinity)");
+                }
+                Console.WriteLine(" x={0} y={1}", x, y);
+               }
+
+        }
+    }
+}
